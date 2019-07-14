068de6a4ec869dfb77c184240502389415783428
Aron Rubin <aronrubin@gmail.com>
2019-07-14 19:13:01 -0400

    Added comments and improved some naming.

--- src/ContainerBench.cpp
+++ src/ContainerBench.cpp
@@ -11,21 +11,26 @@
 
 using LargeThing = std::array<int, 1024>;
 
-struct Untimed {
-  Untimed( benchmark::State &state ) : m_state{state} { m_state.PauseTiming(); }
-  ~Untimed() noexcept { m_state.ResumeTiming(); }
-  benchmark::State &m_state;
-};
-
+/**
+ * @brief Report the couters after a bench run
+ * state.range( 0 ) is assumed to hold the number of items processed
+ * @param state Benchmark state object to populate
+ * @param bytes_per_item The number of bytes in each item for byte rate
+ */
 static void reportCounters( benchmark::State &state, size_t bytes_per_item ) {
   state.SetItemsProcessed( static_cast<int64_t>( state.iterations() ) * state.range( 0 ) );
   state.SetBytesProcessed( static_cast<int64_t>( state.iterations() ) * state.range( 0 ) * bytes_per_item );
   state.counters["size"] = state.range( 0 );
 }
 
-static const std::vector<std::pair<int64_t, int64_t>> g_default_ranges {
-  {1 << 0, 10 << 10},
-  {10 << 10, 10 << 10}
+/**
+ * Value to pass for the bounds to permute benchmark runs
+ * The first range pair is the number of items to process
+ * The second range pair is the initial size of a container
+ */
+static const std::vector<std::pair<int64_t, int64_t>> DEFAULT_RANGES {
+    {1 << 0, 10 << 10},  // number of items to process
+    {10 << 10, 10 << 10} // initial population of the container
 };
 
 /* ---- Insertion Benchmarks ---- */
@@ -33,7 +38,7 @@ static const std::vector<std::pair<int64_t, int64_t>> g_default_ranges {
 template <typename SeqContainer>
 void BM_InsertFront( benchmark::State &state ) {
   typename SeqContainer::value_type value;
-  for ( auto _ : state ) {
+  for ( auto work_iteration : state ) {
     SeqContainer container;
     std::fill_n( std::inserter( container, container.begin() ), state.range( 0 ), value );
   }
@@ -43,7 +48,7 @@ void BM_InsertFront( benchmark::State &state ) {
 template <typename SeqContainer>
 void BM_InsertBack( benchmark::State &state ) {
   typename SeqContainer::value_type value;
-  for ( auto _ : state ) {
+  for ( auto work_iteration : state ) {
     SeqContainer container;
     std::fill_n( std::back_inserter( container ), state.range( 0 ), value );
   }
@@ -53,7 +58,7 @@ void BM_InsertBack( benchmark::State &state ) {
 template <typename SeqContainer>
 void BM_InsertMiddle( benchmark::State &state ) {
   typename SeqContainer::value_type value;
-  for ( auto _ : state ) {
+  for ( auto work_iteration : state ) {
     state.PauseTiming();
     SeqContainer container;
     std::fill_n( std::back_inserter( container ), state.range( 1 ), value );
@@ -70,7 +75,7 @@ void BM_InsertMiddle( benchmark::State &state ) {
 template <typename SeqContainer>
 void BM_RemoveFront( benchmark::State &state ) {
   typename SeqContainer::value_type value;
-  for ( auto _ : state ) {
+  for ( auto work_iteration : state ) {
     state.PauseTiming();
     SeqContainer container;
     std::fill_n( std::back_inserter( container ), state.range( 0 ), value );
@@ -85,7 +90,7 @@ void BM_RemoveFront( benchmark::State &state ) {
 template <typename SeqContainer>
 void BM_RemoveBack( benchmark::State &state ) {
   typename SeqContainer::value_type value;
-  for ( auto _ : state ) {
+  for ( auto work_iteration : state ) {
     state.PauseTiming();
     SeqContainer container;
     std::fill_n( std::back_inserter( container ), state.range( 0 ), value );
@@ -103,7 +108,7 @@ void BM_RemoveMiddle( benchmark::State &state ) {
   if ( state.range( 0 ) > state.range( 1 ) ) {
     state.SkipWithError( "Range to remove is larger than range to initialize" );
   }
-  for ( auto _ : state ) {
+  for ( auto work_iteration : state ) {
     state.PauseTiming();
     SeqContainer container;
     std::fill_n( std::back_inserter( container ), state.range( 1 ) + 1, value );
@@ -126,7 +131,7 @@ void BM_AccessForward( benchmark::State &state ) {
   typename SeqContainer::value_type value;
   SeqContainer container;
   std::fill_n( std::back_inserter( container ), state.range( 0 ), value );
-  for ( auto _ : state ) {
+  for ( auto work_iteration : state ) {
     for ( auto &accessed : container ) {
       if constexpr (std::is_same_v<typename SeqContainer::value_type, int>) {
         benchmark::DoNotOptimize( accessed ^ accessed );
@@ -143,7 +148,7 @@ void BM_AccessBackward( benchmark::State &state ) {
   typename SeqContainer::value_type value;
   SeqContainer container;
   std::fill_n( std::back_inserter( container ), state.range( 0 ), value );
-  for ( auto _ : state ) {
+  for ( auto work_iteration : state ) {
     for ( auto iter = std::rbegin( container ), end = std::rend( container ); iter != end; iter++ ) {
       benchmark::DoNotOptimize( *iter );
     }
@@ -157,7 +162,7 @@ void BM_AccessRandom( benchmark::State &state ) {
   SeqContainer container;
   std::fill_n( std::back_inserter( container ), state.range( 0 ) + 1, value );
   size_t size = state.range( 0 );
-  for ( auto _ : state ) {
+  for ( auto work_iteration : state ) {
     // NOTE this algorithm assumes an even range
     for ( size_t pos = size / 2; pos > 0; pos-- ) {
       auto iter = container.begin();
@@ -171,67 +176,70 @@ void BM_AccessRandom( benchmark::State &state ) {
   reportCounters( state, sizeof( value ) );
 }
 
-BENCHMARK_TEMPLATE( BM_InsertFront, std::vector<int> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_InsertFront, std::list<int> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_InsertFront, std::deque<int> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_InsertFront, std::vector<LargeThing> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_InsertFront, std::list<LargeThing> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_InsertFront, std::deque<LargeThing> )->Ranges( g_default_ranges );
+BENCHMARK_TEMPLATE( BM_InsertFront, std::vector<int> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_InsertFront, std::list<int> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_InsertFront, std::deque<int> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_InsertFront, std::vector<LargeThing> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_InsertFront, std::list<LargeThing> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_InsertFront, std::deque<LargeThing> )->Ranges( DEFAULT_RANGES );
 
-BENCHMARK_TEMPLATE( BM_InsertBack, std::vector<int> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_InsertBack, std::list<int> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_InsertBack, std::deque<int> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_InsertBack, std::vector<LargeThing> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_InsertBack, std::list<LargeThing> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_InsertBack, std::deque<LargeThing> )->Ranges( g_default_ranges );
+BENCHMARK_TEMPLATE( BM_InsertBack, std::vector<int> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_InsertBack, std::list<int> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_InsertBack, std::deque<int> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_InsertBack, std::vector<LargeThing> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_InsertBack, std::list<LargeThing> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_InsertBack, std::deque<LargeThing> )->Ranges( DEFAULT_RANGES );
 
-BENCHMARK_TEMPLATE( BM_InsertMiddle, std::vector<int> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_InsertMiddle, std::list<int> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_InsertMiddle, std::deque<int> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_InsertMiddle, std::vector<LargeThing> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_InsertMiddle, std::list<LargeThing> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_InsertMiddle, std::deque<LargeThing> )->Ranges( g_default_ranges );
+BENCHMARK_TEMPLATE( BM_InsertMiddle, std::vector<int> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_InsertMiddle, std::list<int> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_InsertMiddle, std::deque<int> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_InsertMiddle, std::vector<LargeThing> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_InsertMiddle, std::list<LargeThing> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_InsertMiddle, std::deque<LargeThing> )->Ranges( DEFAULT_RANGES );
 
-BENCHMARK_TEMPLATE( BM_RemoveFront, std::vector<int> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_RemoveFront, std::list<int> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_RemoveFront, std::deque<int> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_RemoveFront, std::vector<LargeThing> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_RemoveFront, std::list<LargeThing> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_RemoveFront, std::deque<LargeThing> )->Ranges( g_default_ranges );
+BENCHMARK_TEMPLATE( BM_RemoveFront, std::vector<int> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_RemoveFront, std::list<int> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_RemoveFront, std::deque<int> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_RemoveFront, std::vector<LargeThing> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_RemoveFront, std::list<LargeThing> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_RemoveFront, std::deque<LargeThing> )->Ranges( DEFAULT_RANGES );
 
-BENCHMARK_TEMPLATE( BM_RemoveBack, std::vector<int> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_RemoveBack, std::list<int> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_RemoveBack, std::deque<int> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_RemoveBack, std::vector<LargeThing> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_RemoveBack, std::list<LargeThing> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_RemoveBack, std::deque<LargeThing> )->Ranges( g_default_ranges );
+BENCHMARK_TEMPLATE( BM_RemoveBack, std::vector<int> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_RemoveBack, std::list<int> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_RemoveBack, std::deque<int> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_RemoveBack, std::vector<LargeThing> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_RemoveBack, std::list<LargeThing> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_RemoveBack, std::deque<LargeThing> )->Ranges( DEFAULT_RANGES );
 
-BENCHMARK_TEMPLATE( BM_RemoveMiddle, std::vector<int> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_RemoveMiddle, std::list<int> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_RemoveMiddle, std::deque<int> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_RemoveMiddle, std::vector<LargeThing> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_RemoveMiddle, std::list<LargeThing> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_RemoveMiddle, std::deque<LargeThing> )->Ranges( g_default_ranges );
+BENCHMARK_TEMPLATE( BM_RemoveMiddle, std::vector<int> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_RemoveMiddle, std::list<int> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_RemoveMiddle, std::deque<int> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_RemoveMiddle, std::vector<LargeThing> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_RemoveMiddle, std::list<LargeThing> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_RemoveMiddle, std::deque<LargeThing> )->Ranges( DEFAULT_RANGES );
 
-BENCHMARK_TEMPLATE( BM_AccessForward, std::vector<int> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_AccessForward, std::list<int> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_AccessForward, std::deque<int> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_AccessForward, std::vector<LargeThing> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_AccessForward, std::list<LargeThing> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_AccessForward, std::deque<LargeThing> )->Ranges( g_default_ranges );
+BENCHMARK_TEMPLATE( BM_AccessForward, std::vector<int> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_AccessForward, std::list<int> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_AccessForward, std::deque<int> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_AccessForward, std::vector<LargeThing> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_AccessForward, std::list<LargeThing> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_AccessForward, std::deque<LargeThing> )->Ranges( DEFAULT_RANGES );
 
-BENCHMARK_TEMPLATE( BM_AccessBackward, std::vector<int> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_AccessBackward, std::list<int> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_AccessBackward, std::deque<int> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_AccessBackward, std::vector<LargeThing> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_AccessBackward, std::list<LargeThing> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_AccessBackward, std::deque<LargeThing> )->Ranges( g_default_ranges );
+BENCHMARK_TEMPLATE( BM_AccessBackward, std::vector<int> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_AccessBackward, std::list<int> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_AccessBackward, std::deque<int> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_AccessBackward, std::vector<LargeThing> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_AccessBackward, std::list<LargeThing> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_AccessBackward, std::deque<LargeThing> )->Ranges( DEFAULT_RANGES );
 
-BENCHMARK_TEMPLATE( BM_AccessRandom, std::vector<int> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_AccessRandom, std::list<int> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_AccessRandom, std::deque<int> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_AccessRandom, std::vector<LargeThing> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_AccessRandom, std::list<LargeThing> )->Ranges( g_default_ranges );
-BENCHMARK_TEMPLATE( BM_AccessRandom, std::deque<LargeThing> )->Ranges( g_default_ranges );
+BENCHMARK_TEMPLATE( BM_AccessRandom, std::vector<int> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_AccessRandom, std::list<int> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_AccessRandom, std::deque<int> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_AccessRandom, std::vector<LargeThing> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_AccessRandom, std::list<LargeThing> )->Ranges( DEFAULT_RANGES );
+BENCHMARK_TEMPLATE( BM_AccessRandom, std::deque<LargeThing> )->Ranges( DEFAULT_RANGES );
 
+/**
+ * Default implementation of main for a benchmark suite
+ */
 BENCHMARK_MAIN();
